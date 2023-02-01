cec42bfba55b7ee331efd1973927000f2e67d857
emilhajizada <emil.hajizada@code.edu.az>
2023-02-01 05:47:10 +0400

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,33 @@
 const imgs = document.querySelectorAll("#testimonials_section img");
-
-
-imgs.forEach(img=>{
-    img.onclick = ()=>{
-        imgs.forEach(img=>{
-            img.classList.remove("active")
-        })
-        img.classList.add("active")
-    }
-})
+const texts = [
+  {
+    text: "“Woman image -Херсонес имеет для нас особое значение, и мы, безусловно, будем его развивать. То, что сделано, совершенно недостаточно. Знаю, что раскопки проводятся и работа идет, но этого пока недостаточно. Мы должны сделать еще больше и будем развивать Херсонес и дальше как центр притяжения не только для граждан нашей страны, но и тех кто захочет приехать в Севастополь из-за границы”",
+    name: "Woman image",
+    job: "Woman image",
+  },
+  {
+    text: "“Херсонес имеет для нас особое значение, и мы, безусловно, будем его развивать. То, что сделано, совершенно недостаточно. Знаю, что раскопки проводятся и работа идет, но этого пока недостаточно. Мы должны сделать еще больше и будем развивать Херсонес и дальше как центр притяжения не только для граждан нашей страны, но и тех кто захочет приехать в Севастополь из-за границы”",
+    name: "В.В. Путин",
+    job: "Президент России",
+  },
+  {
+    text: "Monarx image - Херсонес имеет для нас особое значение, и мы, безусловно, будем его развивать. То, что сделано, совершенно недостаточно. Знаю, что раскопки проводятся и работа идет, но этого пока недостаточно. Мы должны сделать еще больше и будем развивать Херсонес и дальше как центр притяжения не только для граждан нашей страны, но и тех кто захочет приехать в Севастополь из-за границы”",
+    name: "Monarx image",
+    job: "Monarx image",
+  },
+];
+imgs.forEach((img, i) => {
+  img.addEventListener("click", async () => {
+    const item = document.querySelector("#testimonials_section .active");
+    const item_parent = item.parentElement.parentElement.children[1];
+    item_parent.children[0].innerHTML = texts[i].text;
+    item_parent.children[1].innerHTML = texts[i].name;
+    item_parent.children[2].innerHTML = texts[i].job;
+    item.classList.remove("active");
+    img.classList.add("active");
+    await setTimeout(() => {
+      item_parent.classList.add("animate__fadeInUp");
+    }, 100);
+    item_parent.classList.remove("animate__fadeInUp");
+  });
+});
--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,9 +1,15 @@
 const list = document.querySelectorAll('#video_list_section li')
 
 function toggle(n) {
+  const item = list[n]
   const item_arrow = list[n].children[0].children[1]
   const item_img = list[n].children[1]
-  const bool = item_arrow.style.transform == ''
+  const item_play = list[n].children[1].children[1]
+  const bool = item_arrow.style.transform === ''
   item_arrow.style.transform = bool ? 'rotate(180deg)' : ''
-  item_img.style.display = bool ? 'flex' : 'none'
+  item_img.style.visibility = bool ? 'visible' : 'hidden'
+  item_img.style.height = bool ? '410px' : '0'
+  item_img.style.opacity  = bool ? '1' : '0'
+  item_play.style.display = bool ? 'flex' : 'none'
+  item.style.padding = bool ? '32px' : '32px 32px 0 32px'
 }
